9bd0372d37173efd22681290072278d680082951
José Marcos Gomes <jose.marcos.gomes@gmail.com>
2020-12-11 15:13:37 -0300

    Atualizar com a versão certa dos programas

--- experiment/notebook/pre_process.py
+++ experiment/notebook/pre_process.py
@@ -21,23 +21,41 @@ from pycm import ConfusionMatrix, Compare
 import re
 
 
-def experiment(model, X, Y, alg_title, files, report = True):
+def experiment(model, X, Y, alg_title, files, report = True, n = 1):
     
-    t_start = perf_counter()
+    x = []
+    f = []
+    e = []
+    t = []
+    p = []
     
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
+    for i in range(n):
+        t_start = perf_counter()
 
-    model.fit(X_train, Y_train)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
 
-    Y_pred = model.predict(X_test)
-    
-    t_stop = perf_counter()
-    
-    elapsed_time = t_stop - t_start
+        model.fit(X_train, Y_train)
 
-    cm = ConfusionMatrix(actual_vector = Y_test, predict_vector = Y_pred)
+        Y_pred = model.predict(X_test)
 
-    if report:
+        t_stop = perf_counter()
+
+        elapsed_time = t_stop - t_start
+
+        cm = ConfusionMatrix(actual_vector = Y_test, predict_vector = Y_pred)
+        
+        x.append(alg_title)
+        f.append(files)
+        e.append(elapsed_time)
+        t.append(Y_test)
+        p.append(Y_pred)
+        
+    sq = { 'alg': x, 'file': f, 'elapsed_time': e, 'test': t, 'pred': p }
+    df = pd.DataFrame(sq)
+    file_name = re.sub('[\s+]', '', files + '_' + alg_title + '_cm.csv')
+    df.to_csv(file_name, index = False)
+
+    if report and n == 1:
         print(alg_title)
         print(classification_report(Y_test, Y_pred))
 
@@ -46,16 +64,19 @@ def experiment(model, X, Y, alg_title, files, report = True):
         plt.savefig(file_name)
         plt.show()
     
-    return {
-        'elapsed_time': elapsed_time,
-        'test': Y_test,
-        'pred': Y_pred,
-        'cm': cm,
-        'title': alg_title,
-    }
+    if n == 1:
+        return {
+            'elapsed_time': elapsed_time,
+            'test': Y_test,
+            'pred': Y_pred,
+            'cm': cm,
+            'title': alg_title,
+        }
 
 def run_pre_process(data):
     
+    data.drop(data.columns.difference(['n', 'v', 'l', 'd', 'i', 'e', 'b', 't', 'lOCode', 'lOComment', 'lOBlank', 'uniq_Op', 'uniq_Opnd', 'total_Op', 'total_Opnd', 'defects']), 1, inplace=True)
+
     data.groupby('defects')['b'].apply(lambda x: x.count())
 
     sns.histplot(data = data, x = 'defects')
